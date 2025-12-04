4a0e5553d5d714017924010ae747144dfec0a115
rsmalec <rsmalec@kplabs.pl>
2025-12-04 11:41:23 +0100

    vitis app added

---
+++ vitis/spi_test_app/src/spi_header.h
@@ -0,0 +1,26 @@
+#define TESTAPP_GEN
+
+/******************************************************************************
+* Copyright (C) 2007 - 2023 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2022 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef SPI_HEADER_H		/* prevent circular inclusions */
+#define SPI_HEADER_H		/* by using protection macros */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "xil_types.h"
+#include "xil_assert.h"
+#include "xstatus.h"
+
+int SpiSelfTestExample(u16 DeviceId);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
